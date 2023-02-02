f3d112df38bf9288ea1ddadd219211dd51359fd4
Benny Sufrin <bennysuf@gmail.com>
2023-02-02 17:49:29 -0500

    adds route

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-// import './App.css';
-import React, {useState} from "react";
+import React, { useState, useEffect } from "react";
+import { Route, Switch } from "react-router-dom";
+import Login from "./login"
 
 function App() {
+  const [data, setData] = useState([])
+
   return (
-""
+    <div>
+      <Switch>
+        <Route exact path="/login" component={Login} />
+      </Switch>
+    </div>
   );
 }
 
