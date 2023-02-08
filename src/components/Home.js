449d2e5b76bbe551d7e9107cf501f8be725037e3
Benny Sufrin <bennysuf@gmail.com>
2023-02-08 15:25:14 -0500

    removes comments, adds welcome header for user

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,17 +8,17 @@ import Stack from '@mui/material/Stack';
 
 export default function Home({ currentUser }) {
     const [posts, setPosts] = useState([])
-    const [reload, setReload] = useState("")
- 
+    // const [reload, setReload] = useState("")
+
     const history = useHistory()
 
     useEffect(() => {
         fetch("http://localhost:9292/home")
             .then(r => r.json())
             .then(d => setPosts(d))
-    }, [reload])
+    }, [])
 
-    function handleCLick(){
+    function handleCLick() {
         history.push("/home/add-post")
     }
 
@@ -26,28 +26,19 @@ export default function Home({ currentUser }) {
     const cards = posts.map(post => <PostCards key={post.id} post={post} />)
 
     return (
-        <div>
-            <h1 >Home page</h1>
-            <Stack direction="row" spacing={1} onClick={handleCLick}>
-                <Chip label="New post" color="primary" />
-            </Stack>
-            <br />
-            <Route path="/home/add-post" component={CreatePost} />
-            <ul> 
+        <>
+            <h1 style={{ textAlign: "center" }} >Welcome {currentUser.username}</h1>
+            <div style={{ textAlign: "center", marginLeft: "100px", position: "absolute" }}>
+                <Stack direction="row" spacing={1} onClick={handleCLick}>
+                    <Chip label="New post" color="primary" />
+                </Stack>
+                <br />
+                <Route path="/home/add-post" component={CreatePost} />
+            </div>
+            <ul>
                 {cards}
             </ul>
-            {/* CSS to keep the cards from dropping down */}
-        </div>
+        </>
     )
 }
-
-//add username name to top of page
-// create post, in nested route
-
-/*
-TODO
- 
-1. figure out hpw tp do nested routes
-2. fix current user issue
-*/
 
