90161eccc515286c09d1a1067c2b6b7c10af4798
Benny Sufrin <bennysuf@gmail.com>
2023-02-08 16:24:17 -0500

    removes comments

--- src/components/PostCards.js
+++ src/components/PostCards.js
@@ -5,9 +5,6 @@ import Paper from '@mui/material/Paper';
 import Grid from '@mui/material/Grid';
 import Avatar from '@mui/material/Avatar';
 import Typography from '@mui/material/Typography';
-import IconButton from '@mui/material/IconButton';
-import DeleteIcon from '@mui/icons-material/Delete';
-import Stack from '@mui/material/Stack';
 
 const StyledPaper = styled(Paper)(({ theme }) => ({
   backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
@@ -18,16 +15,17 @@ const StyledPaper = styled(Paper)(({ theme }) => ({
 }));
 
 
-export default function AutoGridNoWrap({ post, handleDelete }) {
-  const [user, setUser] = useState([])
+export default function AutoGridNoWrap({ post }) {
+  // const [user, setUser] = useState([])
   const [initial, setInitial] = useState([])
   const { body, created_at, user_id } = post
+
 
   useEffect(() => {
     fetch(`http://localhost:9292/user/${user_id}`)
       .then(r => r.json())
       .then(d => {
-        setUser(d)
+        // setUser(d)
         setInitial(d.username.charAt(0))
       })
   }, [])
@@ -50,11 +48,6 @@ export default function AutoGridNoWrap({ post, handleDelete }) {
             <Typography>{body}</Typography>
             <h5>{created_at.slice(0, 10)}, {created_at.slice(11, 19)}</h5>
           </Grid>
-          {/* <Stack direction="row" alignItems="center" spacing={1} onClick={() => handleDelete(post)}>
-            <IconButton aria-label="delete" disabled color="primary"  >
-              <DeleteIcon />
-            </IconButton>
-          </Stack> */}
         </Grid>
       </StyledPaper>
     </Box>
