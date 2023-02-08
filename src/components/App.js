b24a1ef68cd88ea41352d4cae47f7b8eff346536
Benny Sufrin <bennysuf@gmail.com>
2023-02-08 14:53:08 -0500

    function to update current created and working

--- src/components/App.js
+++ src/components/App.js
@@ -8,22 +8,40 @@ function App() {
   const [currentUser, setCurrentUser] = useState([]) //user logged in //clears when page reloaded, how to fix
   const [userData, setUserData] = useState([]) //all users
 
-  console.log("user", currentUser)
-
+  // console.log("user", currentUser)
+  
   useEffect(() => {
     fetch("http://localhost:9292/login")
-      .then(r => r.json())
-      .then(d => setUserData(d))
+    .then(r => r.json())
+    .then(d => setUserData(d))
   }, [])
+  
+    function userUpdate(user){
+      fetch(`http://localhost:9292/current-user/${user.id}`, {
+        method: 'PATCH',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(
+          {
+            user_id: user.id
+          }
+          ),
+        })
+        .then(r => r.json())
+        .then(d => console.log("user", user, "d", d))
+        
+        fetch("http://localhost:9292/current-user")
+        .then(r => r.json())
+        .then(d => console.log("from CU fetch", d))
+  }
 
   return (
     <div>
       <Switch>
         <Route exact path="/login">
-          <Login setCurrentUser={setCurrentUser} userData={userData} />
+          <Login setCurrentUser={setCurrentUser} userData={userData} userUpdate={userUpdate}/>
         </Route>
         <Route exact path="/login/new-user">
-          <AddUser setCurrentUser={setCurrentUser} userData={userData} />
+          <AddUser setCurrentUser={setCurrentUser} userData={userData} userUpdate={userUpdate}/>
         </Route>
         <Route path="/home" component={Home} currentUser={currentUser}/>
       </Switch>
@@ -33,14 +51,16 @@ function App() {
 
 export default App;
 
-//when logged out, sets currentUser to [], history.push("/login")
+
+
+//when logged out, sets currentUser to [], history.push("/login"), delete user from CurrentUser
 //delete posts only in self posts page
 
 /* 
 Need to work on:
-CreatePost
-CommentCards
-self Post page
+[] CreatePost
+[] CommentCards
+[] self Post page
 
 
 Already finises: 
