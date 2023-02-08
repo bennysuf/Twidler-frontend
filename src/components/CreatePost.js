7f2d6e815a8be955230d7bcf48d893f0d24570c3
Benny Sufrin <bennysuf@gmail.com>
2023-02-08 16:23:13 -0500

    adds states and onchange for new post text, post to create the new post, and button to submit

--- src/components/CreatePost.js
+++ src/components/CreatePost.js
@@ -1,25 +1,62 @@
-import React from 'react'
-import Box from '@mui/material/Box';
+import React, { useState } from 'react'
+import { useHistory } from "react-router-dom";
 import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import SendIcon from '@mui/icons-material/Send';
+import Stack from '@mui/material/Stack';
 
-export default function CreatePost() {
+export default function CreatePost({ currentUser, setReload }) {
+    const [newPost, setNewPost] = useState("")
+    const history = useHistory()
+
+    function handleChange(e) {
+        setNewPost(e.target.value)
+    }
+
+    function handleSubmit(e) {
+        setReload("reload")
+
+        e.preventDefault();
+
+        fetch("http://localhost:9292/posts", {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(
+                {
+                    user_id: currentUser.user_id,
+                    body: newPost
+                }
+            ),
+        })
+            .then(r => r.json())
+            .then(d => {
+                setReload("")
+                history.push("/home")
+                setNewPost("")
+            })
+    }
+
     return (
-        <Box>
+        <form style={{ textAlign: "center" }} onSubmit={handleSubmit}>
             <TextField
                 id="outlined-multiline-static"
                 label="Multiline"
                 multiline
-                rows={4}
-                defaultValue="Default Value"
+                rows={3}
+                onChange={handleChange}
             />
-        </Box>
-        // <h1>hello creation</h1>
+            <Stack direction="row-reverse" spacing={1} >
+                <Button type='submit' variant="contained" endIcon={<SendIcon />} >
+                    Send
+                </Button>
+            </Stack>
+        </form>
     )
 }
 
 /*
 TODO
 
-post new post via fetch when onSubmit clicked
+move button down a drop
 
 */
