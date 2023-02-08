d74c710211e74ad3f9e0e241aa5b5a35888a4ff4
Benny Sufrin <bennysuf@gmail.com>
2023-02-08 15:24:17 -0500

    Allows current user state to be passed down

--- src/components/App.js
+++ src/components/App.js
@@ -5,45 +5,49 @@ import Home from "./Home"
 import AddUser from "./AddUser";
 
 function App() {
-  const [currentUser, setCurrentUser] = useState([]) //user logged in //clears when page reloaded, how to fix
+  const [currentUser, setCurrentUser] = useState({ username: "Loading.." }) //user logged in
   const [userData, setUserData] = useState([]) //all users
 
-  // console.log("user", currentUser)
-  
+
   useEffect(() => {
     fetch("http://localhost:9292/login")
-    .then(r => r.json())
-    .then(d => setUserData(d))
+      .then(r => r.json())
+      .then(d => setUserData(d))
   }, [])
-  
-    function userUpdate(user){
-      fetch(`http://localhost:9292/current-user/${user.id}`, {
-        method: 'PATCH',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify(
-          {
-            user_id: user.id
-          }
-          ),
-        })
-        .then(r => r.json())
-        .then(d => console.log("user", user, "d", d))
-        
-        fetch("http://localhost:9292/current-user")
-        .then(r => r.json())
-        .then(d => console.log("from CU fetch", d))
+
+  function userUpdate(user) { //updates current user and pulls it
+    fetch(`http://localhost:9292/current-user/${user.id}`, {
+      method: 'PATCH',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(
+        {
+          user_id: user.id
+        }
+      ),
+    })
+      .then(r => r.json())
   }
+
+  useEffect(() => {
+    fetch("http://localhost:9292/current-user")
+      .then(r => r.json())
+      .then(d => {
+        setCurrentUser(d[0])
+      })
+  }, [])
 
   return (
     <div>
       <Switch>
         <Route exact path="/login">
-          <Login setCurrentUser={setCurrentUser} userData={userData} userUpdate={userUpdate}/>
+          <Login setCurrentUser={setCurrentUser} userData={userData} userUpdate={userUpdate} />
         </Route>
         <Route exact path="/login/new-user">
-          <AddUser setCurrentUser={setCurrentUser} userData={userData} userUpdate={userUpdate}/>
+          <AddUser setCurrentUser={setCurrentUser} userData={userData} userUpdate={userUpdate} />
         </Route>
-        <Route path="/home" component={Home} currentUser={currentUser}/>
+        <Route path="/home">
+          <Home currentUser={currentUser} />
+        </Route>
       </Switch>
     </div>
   );
