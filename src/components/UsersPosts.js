04e0720d3019dc785128a7218b50851bb832f411
Benny Sufrin <bennysuf@gmail.com>
2023-02-09 15:43:43 -0500

    creates fetch for users posts, maps it, renders it

--- src/components/UsersPosts.js
+++ src/components/UsersPosts.js
@@ -1,7 +1,21 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import PostCards from "./PostCards"
 
-export default function UsersPosts() {
-  return (
-   ""
-  )
+export default function UsersPosts({currentUser}) {
+    const [ownPosts, setOwnPosts] = useState([])
+
+    useEffect(()=> {
+        fetch(`http://localhost:9292/posts/${currentUser.user_id}`)
+        .then(r => r.json())
+        .then(d => setOwnPosts(d))
+    },[])
+
+
+    const cards = ownPosts.map(post => <PostCards key={post.id} post={post}/>)
+
+    return (
+        <div>
+            {cards}
+        </div>
+    )
 }
