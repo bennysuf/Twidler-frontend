b6ee5a6adef34cf35f99fef41d9c15562ee18615
Benny Sufrin <bennysuf@gmail.com>
2023-02-14 21:54:53 -0500

    moves form and button, adds fetch

--- src/components/Edit.js
+++ src/components/Edit.js
@@ -1,38 +1,61 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import Button from '@mui/material/Button';
 import DeleteIcon from '@mui/icons-material/Delete';
 import Stack from '@mui/material/Stack';
 import TextField from '@mui/material/TextField';
 
 
-export default function Edit() {
+export default function Edit({ appBar, currentUser }) {
     const [userUpdate, setUserUpdate] = useState("")
 
-    // function handleChange(e){
-    //     setUserUpdate(e.target.value)
-    // }
+    function handleChange(e){
+        setUserUpdate(e.target.value)
+    }
 
     function handleUpdate(e) {
         e.preventDefault()
 
-
-
+        fetch(`http://localhost:9292/edit-user/${currentUser.user_id}`,{
+            method: 'PATCH',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(
+              {
+                id: currentUser.user_id,
+                username: userUpdate
+              }
+            ),
+          })
+            .then(r => r.json())
+            //needs to update current user as well
+            //App has fetch, find what it on line 59
+    }
+        
+        
+        //fetch PATCH
+    
+    
+    function handleDelete(){
+        // fetch(`http://localhost:9292/edit-user/currentUser.user_id`)
+        //fetch delete
     }
 
     return (
-        <Stack direction="row" spacing={2}>
-            <form onSubmit={handleUpdate}>
-                <br/>
-            <TextField id="outlined-basic" label="Outlined" variant="outlined" />
-            <br/>
-                <Button variant="contained" color="success" type='submit' >
-                    Update
+        <>
+            {appBar}
+            <div style={{ textAlign: "center", margin: "200px" }}>
+                <form onSubmit={handleUpdate}>
+                    <TextField id="outlined-basic" label="New username" variant="outlined" onChange={handleChange}/>
+                    <br />
+                    <Button variant="contained" color="success" type='submit' style={{ margin: "15px" }}>
+                        Update Username
+                    </Button>
+                </form>
+            </div>
+            {/* //TODO move button  */}
+                <Button variant="contained" color="error" startIcon={<DeleteIcon />} style={{textAlign: "center", marginBottom: "10px" }}>
+                    Delete Account
                 </Button>
-            </form>
-            <Button variant="contained" color="error" startIcon={<DeleteIcon />}>
-                Delete Account
-            </Button>
-        </Stack>
+        </>
     )
 }
 
