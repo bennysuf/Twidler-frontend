52461b89193f3b0962bede75f17071289c1cef9d
Benny Sufrin <bennysuf@gmail.com>
2023-02-14 21:24:22 -0500

    removes unused state

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,13 +3,12 @@ import PostCards from "./PostCards"
 
 export default function Home({ appBar }) {
     const [posts, setPosts] = useState("Loading...")
-    const [reload, setReload] = useState("") //* we might not need this, we go back to home page
 
     useEffect(() => { // pulls all posts
         fetch("http://localhost:9292/home")
             .then(r => r.json())
             .then(d => setPosts(d.map(post => <PostCards key={post.id} post={post} />)))
-    }, [reload])
+    }, [])
 
     return (
         <>
